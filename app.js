6f45afb032d1442be365ad4db606036b21e819bf
KayhanTejani <kayhantejani@hotmail.com>
2022-05-13 21:01:31 +0500

    return form data in request body

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('./models/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const Item = require('./models/item.model')
 
  // Express setup
 const app = express();
@@ -12,10 +13,20 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+// Routes
 app.get('/create', (req, res) => {
     res.render("item/addOrEdit", {
         viewTitle: "Create Item Entry"
     });
+});
+
+app.post('/create', (req, res) => {
+    console.log(req.body);
 });
 
 app.listen(3000, () => {
