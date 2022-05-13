8e0145b08e44ea0bf6935e30a9046f9e6c5fdb0f
KayhanTejani <kayhantejani@hotmail.com>
2022-05-13 19:12:47 +0500

    setup of default view/layout

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 require('./models/db');
 
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
  // Express setup
 const app = express();
+
+app.use(express.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.json('Items');
